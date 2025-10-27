ef047b68974da67f4a10180ec9aeddff538f223e
Malika Eryominaclear <malika.eryomina@student.manchester.ac.uk>
2025-10-27 21:12:55 +0000

    First commit

---
+++ model/main.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import pickle as pickle
+
+
+def get_clean_data():
+    data = pd.read_csv('/Users/malikaeryomina/PycharmProjects/Streamlit-cancer/data/data.csv')
+
+    data = data.drop(['Unnamed: 32', 'id'], axis=1)
+    data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B':0})
+
+    return data
+
+def create_model(data):
+    X  = data.drop(['diagnosis'], axis=1)
+    Y = data['diagnosis']
+
+
+    #scale the data
+    scaler  = StandardScaler()
+    X  = scaler.fit_transform(X)
+
+
+    #split the data
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+
+    #train
+    model = LogisticRegression()
+    model.fit(X_train, Y_train)
+
+    #test model
+    y_pred = model.predict(X_test)
+    print("Accuracy: ", accuracy_score(Y_test, y_pred))
+    print("Classification Report: ", classification_report(Y_test, y_pred))
+
+    return model, scaler
+
+def main():
+    data = get_clean_data()
+    model, scaler = create_model(data)
+    with open('/Users/malikaeryomina/PycharmProjects/Streamlit-cancer/model/model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+    with open('/Users/malikaeryomina/PycharmProjects/Streamlit-cancer/model/scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
+
+
+if __name__ == "__main__":
+    main()
